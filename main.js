1e54084f9f4aa922d8670a6a57a9927ebd07bf6f
youssefboujtat123 <166310362+youssefboujtat123@users.noreply.github.com>
2024-04-07 16:21:29 -1100

    Update main.js

--- main.js
+++ main.js
@@ -385,14 +385,14 @@ global.reloadHandler = async function(restatConn) {
 
   // Para cambiar estos mensajes, solo los archivos en la carpeta de language, 
   // busque la clave "handler" dentro del json y cámbiela si es necesario
-  conn.welcome = '👋 ¡Hasta luego!\n@user';
-  conn.bye = '👋 ¡Hasta luego!\n@user';
-  conn.spromote = '*[ ℹ️ ] @user Fue promovido a administrador.*';
-  conn.sdemote = '*[ ℹ️ ] @user Fue degradado de administrador.*';
-  conn.sDesc = '*[ ℹ️ ] La descripción del grupo ha sido modificada.*';
-  conn.sSubject = '*[ ℹ️ ] El nombre del grupo ha sido modificado.*';
-  conn.sIcon = '*[ ℹ️ ] Se ha cambiado la foto de perfil del grupo.*';
-  conn.sRevoke = '*[ ℹ️ ] El enlace de invitación al grupo ha sido restablecido.*';
+  conn.welcome = conn.welcome = '*❆━━━━═⏣⊰🌟⊱⏣═━━━━━❆*\n\n*✧❒↵👑❯منورنا🫂💫*\n\n*املئ الاستمـ📜ـارة* *〖ꪶ→ @user』*\n\n*✧❒↵🍁❯اللقب :『』*\n*ملحوظة: اللقب شخصية من الانمي🍭*\n\n*✧❒↵🌐❯اسم الانمي :『』*\n\n*✧❒↵🔮❯ولد او بنت  :『』*\n\n*✧❒↵🎩❯صورة اللقب :『』*\n\n*✧❒↵🎗️❯من طرف مين :『』*\n\n●━━━ • ━❪🌟❫━ • ━━━●\n\n*عبي الاستماره و منشن احد المشرفين┋🛡️*\n*ارسل صوره اللقب الذي اخترته┋📸*\n*ممنوع بنت تاخذ لقب ولد او العكس┋👩🏻👨🏻*\n\n*❆━━━━═⏣⊰🌟⊱⏣═━━━━━❆*\n*╎𝑴𝑨𝑺𝑻𝑬𝑹 ◜🌟◞*' 
+  conn.spromote = '*@user اصبح مشرف !!*'
+  conn.sdemote = '*@user تم تنزيله من مشرف الى عضو !!*'
+  conn.sDesc = '*تم تعديل وصف المجموعه*\n\n*الوصف الجديد:* @desc'
+  conn.sSubject = '*تم تعديل اسم المجموعه*\n*الاسم الجديد:* @subject'
+  conn.sIcon = '*تم تعديل صورة المجموعه!!*'
+  conn.sRevoke = '*تم تحديث رابط دخول المجموعه!!*\n*الرابط الجديد:* @revoke';
+
 
   conn.handler = handler.handler.bind(global.conn);
   conn.participantsUpdate = handler.participantsUpdate.bind(global.conn);
