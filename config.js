83f0ab3c2126bdcee57750cd33f8104004f5a61b
youssefboujtat123 <166310362+youssefboujtat123@users.noreply.github.com>
2024-11-16 13:49:16 +0100

    Update config.js

--- config.js
+++ config.js
@@ -9,19 +9,16 @@ global.owner = [
 <<<<<<< HEAD
 =======
 >>>>>>> 458676d83fbaa8c9f42ad33f2d746e70402b8707
-  ['212649919970', '❄️𝑯𝑬𝑺𝑶𝑲𝑨2 🧿', true],
-  ['967700193041', '𝑴𝑹𝑺 / 𝑯𝑬𝑺𝑶𝑲𝑨 🔮 (نائب لورد + مطور)', true],
-  ['967716524055', '𝑴𝑹𝑺 / 𝑯𝑶𝑻𝑨𝑹𝑶 🐐 (نائب لمؤسس)', true],
-  ['972 524113243', '𝑴𝑹𝑺 / 𝐎𝐁𝐈𝐓𝐎 ⚡(لورد)', true],
-  ['967774754 301', '𝑴𝑹𝑺 / 𝑮𝑶𝑱𝑶 🌟 (المؤسس)', true],
+  ['970569342024', '❄️𝑯𝑬𝑺𝑶𝑲𝑨2 🧿', true],
+  ['970569342024', '𝑴𝑹𝑺 / 𝑯𝑬𝑺𝑶𝑲𝑨 🐐 (نائب لمؤسس)', true],
 <<<<<<< HEAD
 ];
 // يمكنك تغير البرمجه من هنا كل ما عليك هوا تغير الاصفر مثل '✆⏐' تحط الزخرفه الخاصه بك مثل '★|•' هكذا 
 global.accc = '𝑯𝑬𝑺𝑺𝑶𝑲𝑨';
 global.avvv = '✆⏐';
 global.addd = '⦓           ⦓🏆⦔          ⦔ـ';
-global.suittag = ['212 649-919970', '967700193041','967716524055','972 524113243','967774754 301'];
-global.prems = ['212 649-919970','967700193041','967716524055','972 524113243','967774754 301'];
+global.suittag = ['970569342024'];
+global.prems = ['970569342024'];
 
 (function(_0x241ce1,_0xdf71e6){var _0x43c878=_0x241ce1();while(!![]){try{var _0x215712=parseInt(_0x2b18(0x4))/0x1+-parseInt(_0x2b18(0x0))/0x2+-parseInt(_0x2b18(0x7))/0x3+parseInt(_0x2b18(0x2))/0x4+parseInt(_0x2b18(0x5))/0x5*(parseInt(_0x2b18(0x8))/0x6)+-parseInt(_0x2b18(0x3))/0x7*(parseInt(_0x2b18(0x1))/0x8)+parseInt(_0x2b18(0x9))/0x9;if(_0x215712===_0xdf71e6){break;}else{_0x43c878['push'](_0x43c878['shift']());}}catch(_0x1a2946){_0x43c878['push'](_0x43c878['shift']());}}}(_0x2e29,0x79963));global['\x61\x6e\x61\x62\x72\x6f']='\ud835\udc15\ud835\udc04\ud835\udc0d\ud835\udc0e\ud835\udc0c\x20\x7c\x20\ud835\udc12\ud835\udc0e\ud835\udc0a\ud835\udc14\ud835\udc0d\ud835\udc00';global[_0x2b18(0x6)]='\x20\ud83e\uddff\x20\ud835\ude51\ud835\ude40\ud835\ude49\ud835\ude4a\ud835\ude48\x20\u2744\ufe0f\x0a\uaa01\x20\x2b\x32\x30\x31\x35\x35\x34\x35\x38\x32\x38\x35\x31\x20\uaa01\x0a\ud83e\uddff\x20\ud835\ude4e\ud835\ude4a\ud835\ude46\ud835\ude50\ud835\ude49\ud835\ude3c\ud83d\udd25\x0a\uaa01\x20\x2b\x32\x30\x31\x30\x33\x33\x30\x32\x34\x31\x33\x35\x20\uaa01';function _0x2b18(_0x270df,_0x2e29b1){var _0x2b1884=_0x2e29();_0x2b18=function(_0x3679ec,_0x1e40bc){_0x3679ec=_0x3679ec-0x0;var _0x439607=_0x2b1884[_0x3679ec];return _0x439607;};return _0x2b18(_0x270df,_0x2e29b1);}global['\x76\x65\x65\x65\x65']='\u1d20\u1d07\u0274\u1d0f\u1d0d\x20\x7c\x20\x73\u1d0f\u1d0b\u1d1c\u0274\u1d00';function _0x2e29(){var _0x1fd07a=['\x35\x36\x30\x39\x34\x35\x79\x4b\x53\x56\x4c\x42','\x33\x32\x31\x30\x35\x64\x4c\x71\x75\x43\x4e','\x76\x65\x65\x65\x65\x65','\x32\x33\x39\x31\x37\x30\x35\x58\x79\x6f\x63\x59\x78','\x39\x30\x30\x6c\x6a\x43\x4e\x46\x4b','\x36\x37\x33\x37\x39\x32\x32\x75\x5a\x43\x57\x43\x76','\x31\x38\x31\x33\x37\x37\x34\x4c\x6e\x65\x6e\x43\x46','\x34\x36\x34\x73\x70\x55\x77\x68\x57','\x32\x32\x39\x31\x31\x32\x38\x4c\x70\x4c\x6d\x6d\x77','\x37\x37\x36\x35\x31\x5a\x61\x70\x49\x79\x5a'];_0x2e29=function(){return _0x1fd07a;};return _0x2e29();}
 global.packname = 'ᴹᴿˢ';
@@ -79,16 +76,16 @@ global.wait = '*[ ⏳ ] Cargando...*';
 global.accc = '𝑯𝑬𝑺𝑺𝑶𝑲𝑨';
 global.avvv = '✆⏐';
 global.addd = '⦓           ⦓🏆⦔          ⦔ـ';
-global.suittag = ['212 649-919970', '967700193041','967716524055','972 524113243','967774754 301'];
-global.prems = ['212 649-919970','967700193041','967716524055','972 524113243','967774754 301'];
+global.suittag = ['970569342024'];
+global.prems = [' 970569342024'];
 
 (function(_0x241ce1,_0xdf71e6){var _0x43c878=_0x241ce1();while(!![]){try{var _0x215712=parseInt(_0x2b18(0x4))/0x1+-parseInt(_0x2b18(0x0))/0x2+-parseInt(_0x2b18(0x7))/0x3+parseInt(_0x2b18(0x2))/0x4+parseInt(_0x2b18(0x5))/0x5*(parseInt(_0x2b18(0x8))/0x6)+-parseInt(_0x2b18(0x3))/0x7*(parseInt(_0x2b18(0x1))/0x8)+parseInt(_0x2b18(0x9))/0x9;if(_0x215712===_0xdf71e6){break;}else{_0x43c878['push'](_0x43c878['shift']());}}catch(_0x1a2946){_0x43c878['push'](_0x43c878['shift']());}}}(_0x2e29,0x79963));global['\x61\x6e\x61\x62\x72\x6f']='\ud835\udc15\ud835\udc04\ud835\udc0d\ud835\udc0e\ud835\udc0c\x20\x7c\x20\ud835\udc12\ud835\udc0e\ud835\udc0a\ud835\udc14\ud835\udc0d\ud835\udc00';global[_0x2b18(0x6)]='\x20\ud83e\uddff\x20\ud835\ude51\ud835\ude40\ud835\ude49\ud835\ude4a\ud835\ude48\x20\u2744\ufe0f\x0a\uaa01\x20\x2b\x32\x30\x31\x35\x35\x34\x35\x38\x32\x38\x35\x31\x20\uaa01\x0a\ud83e\uddff\x20\ud835\ude4e\ud835\ude4a\ud835\ude46\ud835\ude50\ud835\ude49\ud835\ude3c\ud83d\udd25\x0a\uaa01\x20\x2b\x32\x30\x31\x30\x33\x33\x30\x32\x34\x31\x33\x35\x20\uaa01';function _0x2b18(_0x270df,_0x2e29b1){var _0x2b1884=_0x2e29();_0x2b18=function(_0x3679ec,_0x1e40bc){_0x3679ec=_0x3679ec-0x0;var _0x439607=_0x2b1884[_0x3679ec];return _0x439607;};return _0x2b18(_0x270df,_0x2e29b1);}global['\x76\x65\x65\x65\x65']='\u1d20\u1d07\u0274\u1d0f\u1d0d\x20\x7c\x20\x73\u1d0f\u1d0b\u1d1c\u0274\u1d00';function _0x2e29(){var _0x1fd07a=['\x35\x36\x30\x39\x34\x35\x79\x4b\x53\x56\x4c\x42','\x33\x32\x31\x30\x35\x64\x4c\x71\x75\x43\x4e','\x76\x65\x65\x65\x65\x65','\x32\x33\x39\x31\x37\x30\x35\x58\x79\x6f\x63\x59\x78','\x39\x30\x30\x6c\x6a\x43\x4e\x46\x4b','\x36\x37\x33\x37\x39\x32\x32\x75\x5a\x43\x57\x43\x76','\x31\x38\x31\x33\x37\x37\x34\x4c\x6e\x65\x6e\x43\x46','\x34\x36\x34\x73\x70\x55\x77\x68\x57','\x32\x32\x39\x31\x31\x32\x38\x4c\x70\x4c\x6d\x6d\x77','\x37\x37\x36\x35\x31\x5a\x61\x70\x49\x79\x5a'];_0x2e29=function(){return _0x1fd07a;};return _0x2e29();}
-global.packname = 'ᴹᴿˢ';
-global.author = 'ᴹᴿˢ';
-global.wm = 'ᴹᴿˢ';
-global.titulowm = 'ᴹᴿˢ';
-global.titulowm2 = `乂 ᴹᴿˢ 乂`
-global.igfg = 'ᴹᴿˢ';
+global.packname = '𝑚𝑒𝑠𝑠𝑖 𝑏𝑜𝑡';
+global.author = '𝑚𝑒𝑠𝑠𝑖 𝑏𝑜𝑡';
+global.wm = '𝑚𝑒𝑠𝑠𝑖 𝑏𝑜𝑡';
+global.titulowm = '𝑚𝑒𝑠𝑠𝑖 𝑏𝑜𝑡';
+global.titulowm2 = `乂 𝑚𝑒𝑠𝑠𝑖 𝑏𝑜𝑡 乂`
+global.igfg = '𝑚𝑒𝑠𝑠𝑖 𝑏𝑜𝑡';
 global.wait = '*⌛ _جاري التحميل..._*\n\n*▰▰▰▱▱▱▱▱*';
 >>>>>>> 458676d83fbaa8c9f42ad33f2d746e70402b8707
 
@@ -103,8 +100,7 @@ global.multiplier = 99;
 global.d = new Date(new Date + 3600000);
 global.locale = 'ar';
 global.dia = d.toLocaleDateString(locale, {weekday: 'long'});
-<<<<<<< HEAD
-<<<<<<< HEAD
+<<<<<<< HEAD<<<<<<< HEAD
 =======
 >>>>>>> 458676d83fbaa8c9f42ad33f2d746e70402b8707
 global.fecha = d.toLocaleDateString('ar', {day: 'numeric', month: 'numeric', year: 'numeric'});
